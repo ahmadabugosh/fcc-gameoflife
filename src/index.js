b74cdd07044ee2d3814babeb5b82ba4671f91468
Ahmad Abugosh <ahmadabugosh@gmail.com>
2017-11-21 23:55:56 +0400

    Added template and css

--- src/index.js
+++ src/index.js
@@ -6,10 +6,34 @@ class Grid extends React.Component {
 
 render()
 {
+	const width=this.props.cols *14;
+	const rowsArr=[];
+
+	var boxClass="";
+
+
+	for (var i=0; i<this.pros.rows; i++)
+	{
+		for (var j=0; j<this.pros.rows; j++)
+		{
+				let boxId=i +"_"+j;
+				boxClass=this.props.gridFull[i][j] ? "box on": "box off" ;
+				rowsArr.push(
+					<Box
+						boxClass={}
+
+					)
+		}
+
+	}
+
 
 return (
 
-	<div> Grid</div>
+	<div className="grid" style={{width:width}}	>]
+{{rowsArr}}
+
+	</div>
 );
 }
 
@@ -19,8 +43,12 @@ class Main extends React.Component {
 
 	constructor(){
 		super();
+		this.speed=100;
+		this.rows=30;
+		this.cols=50;
 		this.state={
-			generation:0
+			generation:0,
+			gridFull: Array(this.rows).fill(Array(this.cols).fill(false)
 		}
 
 	}
@@ -37,7 +65,7 @@ return (
 
 	<h1>The Game Of Life</h1>
 
-	<Grid />
+	<Grid gridFull={this.state.gridFull} rows={this.state.rows} cols={this.state.cols} />
 	<h2>Generations: {this.state.generation}</h2>
 
 
